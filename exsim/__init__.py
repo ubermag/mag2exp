39b3383674484f2e7737d332f2d2b4779294f2f7
Martin Lang <m.lang@soton.ac.uk>
2021-07-05 13:37:14 +0200

    Package version and test setup.

--- exsim/__init__.py
+++ exsim/__init__.py
@@ -1,1 +1,22 @@
 from .ltem import ltem_phase
+import pytest
+import importlib
+
+
+__version__ = importlib.metadata.version(__name__)
+
+
+def test():
+    """Run all package tests.
+
+    Examples
+    --------
+    1. Run all tests.
+
+    >>> import exsim
+    ...
+    >>> # exsim.test()
+
+    """
+    return pytest.main(['-v', '--pyargs',
+                        'exsim', '-l'])  # pragma: no cover
