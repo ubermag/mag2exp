159a1abd27b7ffdec2b9387f544c332ef1ec8e2b
Martin Lang <m.lang@soton.ac.uk>
2021-07-08 12:13:44 +0200

    First tests.

---
+++ exsim/tests/test_ltem.py
@@ -0,0 +1,15 @@
+import pytest
+import discretisedfield as df
+import exsim
+
+
+def test_relativistic_wavelength():
+    assert exsim.ltem._relativistic_wavelength(0) == float('inf')
+
+
+def test_ltem_phase():
+    mesh = df.Mesh(p1=(0, 0, 0), p2=(10, 10, 1), cell=(1, 1, 1))
+    field = df.Field(mesh, dim=3, value=(0, 0, 1))
+    phase, ft_phase = exsim.ltem_phase(field)
+    assert (phase.real.array == 0).all()
+    assert (phase.imag.array == 0).all()
