ae73bd72eb1009acf1e64bb2c6caab8480ecd3c9
Martin Lang <m.lang@soton.ac.uk>
2021-07-09 15:16:11 +0200

    More tests.

--- exsim/tests/test_ltem.py
+++ exsim/tests/test_ltem.py
@@ -4,12 +4,20 @@ import exsim
 
 
 def test_relativistic_wavelength():
-    assert exsim.ltem._relativistic_wavelength(0) == float('inf')
+    assert exsim.ltem.relativistic_wavelength(0) == float('inf')
 
 
 def test_ltem_phase():
     mesh = df.Mesh(p1=(0, 0, 0), p2=(10, 10, 1), cell=(1, 1, 1))
     field = df.Field(mesh, dim=3, value=(0, 0, 1))
-    phase, ft_phase = exsim.ltem_phase(field)
+    phase, ft_phase = exsim.ltem.phase(field)
     assert (phase.real.array == 0).all()
     assert (phase.imag.array == 0).all()
+
+
+def test_defocus_image():
+    mesh = df.Mesh(p1=(0, 0, 0), p2=(10, 10, 1), cell=(1, 1, 1))
+    field = df.Field(mesh, dim=3, value=(0, 0, 1))
+    phase, ft_phase = exsim.ltem.phase(field)
+    with pytest.raises(RuntimeError):
+        exsim.ltem.defocus_image(phase)
