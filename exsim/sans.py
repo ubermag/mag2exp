eac328a25af856490a3a417b9d8e7cebb955abbf
samjrholt <s.j.r.holt@warwick.ac.uk>
2021-07-13 10:53:27 +0100

    Correct integrals

--- exsim/sans.py
+++ exsim/sans.py
@@ -96,7 +96,7 @@ def _Q_parallel(field, /, theta=0):
     discretisedfield.Field
         Magnetic interaction vector.
     """
-    m_p_ft = field.integral(direction='z').fft2()
+    m_p_ft = (field * df.dz).integral(direction='z').fft2()
     Qx = -m_p_ft.x * np.sin(theta)**2 + m_p_ft.y * np.cos(theta) * np.sin(theta)
     Qy = -m_p_ft.y*np.cos(theta)**2 + m_p_ft.x*np.cos(theta)*np.sin(theta)
     Qz = m_p_ft.z
@@ -117,7 +117,7 @@ def _Q_perpendicular(field, /, theta=0):
     discretisedfield.Field
         Magnetic interaction vector.
     """
-    m_p_ft = field.integral(direction='z').fft2()
+    m_p_ft = (field * df.dz).integral(direction='z').fft2()
     Qx = -m_p_ft.x
     Qy = -m_p_ft.y*np.cos(theta)**2 + m_p_ft.z*np.cos(theta)*np.sin(theta)
     Qz = m_p_ft.y*np.cos(theta)*np.sin(theta) - m_p_ft.z*np.sin(theta)**2
