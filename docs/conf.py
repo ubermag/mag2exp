8935915b1189d246e3838b7637528b4e9f2b396a
Martin Lang <m.lang@soton.ac.uk>
2021-07-13 13:51:17 +0200

    Templates for documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,12 @@ author = 'Sam Holt, Martin Lang, Marijan Beg, and Hans Fangohr'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.mathjax',
+    'nbsphinx',
+    'm2r2'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -44,9 +50,29 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
+
+# Custom configuration
+autosummary_generate = True
+autosummary_imported_members = True
+autosummary_context = {
+    'excluded_members': ['__radd__', '__rand__', '__rlshift__', '__rmatmul__',
+                         '__rmul__', '__rsub__', '__rtruediv__', '__delattr__',
+                         '__format__', '__ge__', '__getattribute__', '__gt__',
+                         '__init__', '__init_subclass__', '__le__', '__lt__',
+                         '__ne__', '__new__', '__reduce__', '__reduce_ex__',
+                         '__setattr__', '__sizeof__', '__str__',
+                         '__subclasshook__', '__hash__']
+}
+
+autoclass_content = 'class'
+autodoc_inherit_docstrigs = True
+autodoc_default_options = {
+    'member-order': 'groupwise',
+    'exclude-members': '__init__, __weakref__'
+}
