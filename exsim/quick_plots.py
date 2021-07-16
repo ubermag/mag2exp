6e2c3a692ee5167a6c7a0a2535adf5e3b8f30473
samjrholt <s.j.r.holt@warwick.ac.uk>
2021-07-16 14:41:39 +0100

    Change Tikhonov filter

--- exsim/quick_plots.py
+++ exsim/quick_plots.py
@@ -4,15 +4,15 @@ import ubermagutil.units as uu
 from . import ltem
 
 
-def ltem_phase(field, /, kx=0.1, ky=0.1):
-    phase, _ = ltem.phase(field, kx=kx, ky=ky)
+def ltem_phase(field, /, kcx=0.1, kcy=0.1):
+    phase, _ = ltem.phase(field, kcx=kcx, kcy=kcy)
     phase.real.mpl_scalar(cmap='gray',
                           interpolation='spline16',
                           colorbar_label=r'$\phi$ (radians)')
 
 
-def ltem_ft_phase(field, /, kx=0.1, ky=0.1):
-    _, ft_phase = ltem.phase(field, kx=kx, ky=ky)
+def ltem_ft_phase(field, /, kcx=0.1, kcy=0.1):
+    _, ft_phase = ltem.phase(field, kcx=kcx, kcy=kcy)
     fig, ax = plt.subplots()
     (ft_phase.conjugate * ft_phase).plane('z').real.mpl_scalar(
         ax=ax, cmap='gray', interpolation='spline16',
@@ -20,8 +20,8 @@ def ltem_ft_phase(field, /, kx=0.1, ky=0.1):
     multiplier = uu.si_max_multiplier(ft_phase.mesh.region.edges)
     ax.add_patch(matplotlib.patches.Ellipse(
         xy=(0, 0),
-        width=ft_phase.mesh.cell[0] * kx * 2 / multiplier,
-        height=ft_phase.mesh.cell[1] * ky * 2 / multiplier,
+        width=ft_phase.mesh.cell[0] * kcx * 2 / multiplier,
+        height=ft_phase.mesh.cell[1] * kcy * 2 / multiplier,
         edgecolor='red',
         facecolor='none',
         linewidth=3,
@@ -29,9 +29,9 @@ def ltem_ft_phase(field, /, kx=0.1, ky=0.1):
     ax.legend(frameon=True)
 
 
-def ltem_defocus(field, /, kx=0.1, ky=0.1,
+def ltem_defocus(field, /, kcx=0.1, kcy=0.1,
                  Cs=0, df_length=0.2e-3, U=None, wavelength=None):
-    phase, _ = ltem.phase(field, kx=kx, ky=ky)
+    phase, _ = ltem.phase(field, kcx=kcx, kcy=kcy)
     defocus = ltem.defocus_image(phase, Cs=Cs, df_length=df_length,
                                  U=U, wavelenght=wavelength)
     defocus.mpl_scalar(cmap='gray', interpolation='spline16',
