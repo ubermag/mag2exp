7737bd4ddea2cc07ac422cbe9d480bdbb58c5dfe
samjrholt <s.j.r.holt@warwick.ac.uk>
2021-07-16 15:01:48 +0100

    Add Neel and Bloch tests

--- exsim/tests/test_ltem.py
+++ exsim/tests/test_ltem.py
@@ -14,6 +14,32 @@ def test_ltem_phase():
     assert (phase.real.array == 0).all()
     assert (phase.imag.array == 0).all()
 
+def test_ltem_phase_neel():
+    mesh = df.Mesh(p1=(0, 0, 0), p2=(10, 10, 1), cell=(1, 1, 1))
+    def f_val(position):
+        x, y, z = position
+        if x < 5:
+            return (1, 0, 0)
+        else:
+            return (-1, 0, 0)
+    field = df.Field(mesh, dim=3, value=f_val)
+    phase, ft_phase = exsim.ltem.phase(field)
+    assert (phase.real.array == 0).all()
+    assert (phase.imag.array == 0).all()
+
+def test_ltem_phase_bloch():
+    mesh = df.Mesh(p1=(0, 0, 0), p2=(10, 10, 1), cell=(1, 1, 1))
+    def f_val(position):
+        x, y, z = position
+        if x < 5:
+            return (0, 1, 0)
+        else:
+            return (0, -1, 0)
+    field = df.Field(mesh, dim=3, value=f_val)
+    phase, ft_phase = exsim.ltem.phase(field)
+    assert (phase.real.array != 0).all()
+    assert (phase.imag.array != 0).all()
+
 
 def test_defocus_image():
     mesh = df.Mesh(p1=(0, 0, 0), p2=(10, 10, 1), cell=(1, 1, 1))
