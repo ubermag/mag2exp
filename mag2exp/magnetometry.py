b2a16598c652f67d1c874af834f9f5fd19a9805b
samjrholt <s.j.r.holt@warwick.ac.uk>
2021-07-27 21:24:20 +0100

    Add torque

--- mag2exp/magnetometry.py
+++ mag2exp/magnetometry.py
@@ -4,6 +4,8 @@ Module for calculation of magnetometry based techniques.
 """
 import mag2exp
 import discretisedfield as df
+import micromagneticmodel as mm
+import oommfc as oc
 
 
 def magnetisation(field):
@@ -23,3 +25,29 @@ def magnetisation(field):
     norm_field = df.Field(field.mesh, dim=1, value=(field.norm.array != 0))
     volume = df.integral(norm_field * df.dV, direction='xyz')
     return (df.integral(field * df.dV / volume, direction='xyz'))
+
+
+def torque(system):
+    r""" Calculation of the torque.
+
+    Parameters
+    ----------
+    system : micromagneticmodel.System
+        Micromagnetic system which must include the magnetisation
+        configuration and an energy equation which includes demagnetisation.
+
+    Returns
+    -------
+    tuple
+        Torque in :math:`\textrm{Nm}^{-2}`.
+
+    """
+    total_field = (mm.consts.mu0 *
+                   (oc.compute(system.energy.demag.effective_field, system) +
+                    system.energy.zeeman.H))
+    norm_field = df.Field(system.m.mesh, dim=1,
+                          value=(system.m.norm.array != 0))
+    volume = df.integral(norm_field * df.dV, direction='xyz')
+    moment = system.m * volume
+    torque = (moment & total_field)
+    return (df.integral(torque * df.dV / volume**2, direction='xyz'))
