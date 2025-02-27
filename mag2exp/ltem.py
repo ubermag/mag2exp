b6db3cf3252933b567f2f7e2ce6952308ae9c2bf
Sam Holt <samjrholt@gmail.com>
2025-02-27 16:13:39 +0100

    edit docsting to deal with scipy updated constants

--- mag2exp/ltem.py
+++ mag2exp/ltem.py
@@ -336,7 +336,7 @@ def relativistic_wavelength(voltage):
 
     >>> import mag2exp
     >>> mag2exp.ltem.relativistic_wavelength(300e3)
-    1.9687489006848795e-12
+    1.9687...e-12
 
     """
     return (
